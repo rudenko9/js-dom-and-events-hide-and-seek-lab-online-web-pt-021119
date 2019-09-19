8987700604f959dab4b835c6009f62dff7b3f038
Nataliya R <n.rudenko@gmail.com>
2019-09-19 17:59:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ const deepestChild = function() {
   return node;
 }
 
+const increaseRankBy = function(n) {
+  const list = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i < list.length; i++) {
+    let children = list[i].children;
+    for (let j = 0; j < children.length; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
