c562eaecc7118ec70d5ecbebb1965b1407d9c310
Nataliya R <n.rudenko@gmail.com>
2019-09-19 17:58:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const getFirstSelector = function (selector) {
   return document.querySelector(selector);
 }
 
+const nestedTarget = function() {
+  return document.querySelector('#nested .target')
+}
